84a14acc2e913aee63204b56febdbfded460b2ef
Varghese Baby <vbpoulose@gmail.com>
2025-08-22 18:27:09 -0400

    Bash: Test

---
+++ hello_copy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Hello, World!"
+echo My first bash text
+# To open vim editor vim.file name example vim.hello.sh
+# To enter text first Press i -- Insert -- below it will display meaning ready to enter text 
+# To Save & exit press ESC :wq
+# To Quit with out saving :q!
