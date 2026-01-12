3676369d997a178237325993c29a85697726b8cc
Varghese Baby <vbpoulose@gmail.com>
2026-01-11 20:39:57 -0500

    Linux: BashScript4voter.sh

---
+++ voter.sh
@@ -0,0 +1,38 @@
+#!/bin/bash 
+
+
+echo Hi varghese,Hope Your doing well..Go to this path for learning basic /home/varghese/bash_varghese
+
+
+# Ask for user input
+read -p "Enter your First Name: " First_Name
+read -p "Enter your Last Name: " Last_Name
+read -p "Enter your Age: " Age
+read -p "Enter your Location: " Location
+
+
+
+# Condition To check Eligibility
+
+if [ "$Age" -ge 18 ] 
+
+then
+	echo "Allow to vote"
+else
+	echo "Dont Allow"
+fi
+
+# Display info nicely
+echo ""
+echo "=============================="
+echo "       Display Voter Info     "
+echo "=============================="
+echo "First Name : $First_Name"
+echo "Last Name  : $Last_Name"
+echo " Age 	 : $Age" 
+echo "Location   : $Location"
+echo "=============================="
+
+
+
+
