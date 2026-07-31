aa2d01c19118234c9a5549c220ffe0b53770052c
Varghese Baby <Vbpoulose@gmail.com>
2026-07-31 13:26:51 -0400

    Auto backup 2026-07-31 13:26:51

--- sheBangscript22July2026/disk_check.sh
+++ sheBangscript22July2026/disk_check.sh
@@ -1,3 +1,31 @@
-#!/bas/bin
+#!/bin/bash
 #
-#Dish Usage
+#Dish Usage command
+#
+
+
+
+usage=`df -h | grep "efivarfs" | awk '{print $5}' | sed 's|%||g'`
+
+echo "Current Disk Usage : $usage"
+
+if [ $usage -lt 60 ]; then
+	echo "Disk Usage is Normal: $usage%"
+
+elif [ $usage -lt 80 ]; then
+	echo "Disk Usage is Warning: $usage%"
+elif [ $usage -lt 90 ]; then
+        echo "Disk Usage is Critical: $usage%"
+	echo "Take Immediate Action"
+
+else
+	echo "Disk Usage is Critical: $usage%"
+	echo "Take Immediate Action"
+
+fi
+
+
+
+
+
+
---
+++ sheBangscript22July2026/for_loop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#for  i in 1 2 3 4 5
+#do
+#	 echo $i
+# done
+#
+#
+for user in varghese root
+ do
+	 echo ""
+	 echo $user
+	 id $user
+	 echo ""
+ done
